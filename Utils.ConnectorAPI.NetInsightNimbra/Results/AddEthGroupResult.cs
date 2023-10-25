260ada79d2f889dcbd950f31a36a66dc0129da28
Jason <jason.wan@skyline.be>
2023-10-25 12:12:34 +0800

    Updated missing comments

--- Utils.ConnectorAPI.NetInsightNimbra/Results/AddEthGroupResult.cs
+++ Utils.ConnectorAPI.NetInsightNimbra/Results/AddEthGroupResult.cs
@@ -4,10 +4,18 @@
     using System;
 
     /// <summary>
-    /// Represents the result after adding the ETH Group
+    /// Represents the result after adding the Ethernet Group.
     /// </summary>
     public class AddEthGroupResult : BaseResult
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddEthGroupResult"/> class.
+        /// </summary>
+        /// <param name="id">The unique identifier of the result.</param>
+        /// <param name="groupKey">The unique identifier of the group.</param>
+        /// <param name="groupId">The ID of the Ethernet group.</param>
+        /// <param name="success">A value indicating whether the operation was successful.</param>
+        /// <param name="error">An error message, if applicable.</param>
         public AddEthGroupResult(Guid id, string groupKey, string groupId, bool success, string error = null)
         {
             this.Id = id;
@@ -19,15 +27,16 @@
         }
 
         /// <summary>
-        /// Represents the unique identifier of the group.
+        /// Gets or sets the unique identifier of the group.
         /// </summary>
         [JsonProperty("Group Key")]
         public string GroupKey { get; set; }
 
         /// <summary>
-        /// Represents the ID of the ETH group.
+        /// Gets or sets the ID of the Ethernet group.
         /// </summary>
         [JsonProperty("Group ID")]
         public string GroupID { get; set; }
     }
+
 }
